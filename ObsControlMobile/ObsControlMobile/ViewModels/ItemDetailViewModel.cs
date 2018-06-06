ab7117229be86d034ef27ce17829c852b493dd48
bemchenko <bemchenko@DELLXPS13_2>
2018-06-06 13:02:23 +0300

    Alpha

---
+++ ObsControlMobile/ObsControlMobile/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using ObsControlMobile.Models;
+
+namespace ObsControlMobile.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
