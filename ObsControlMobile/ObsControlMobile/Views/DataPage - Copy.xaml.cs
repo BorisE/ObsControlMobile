8f8c1c75583e64b9730c7cf9e73cd223e455c2c8
bemchenko <bemchenko@DELLXPS13_2>
2018-06-10 04:08:22 +0300

    1.0.2c

---
+++ ObsControlMobile/ObsControlMobile/Views/DataPage - Copy.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using ObsControlMobile.Models;
+using ObsControlMobile.Views;
+using ObsControlMobile.ViewModels;
+
+namespace ObsControlMobile.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class DataPage : ContentPage
+	{
+        ItemsViewModel viewModel;
+
+        public DataPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+    }
+}
