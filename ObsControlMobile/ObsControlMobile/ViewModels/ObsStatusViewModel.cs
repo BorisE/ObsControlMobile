a535c5047996d93ef3629a5049e4d2cf2bdb98d3
bemchenko <bemchenko@DELLXPS13_2>
2018-07-01 03:12:38 +0300

    1.2 beta1 not workin after 3am? check

--- ObsControlMobile/ObsControlMobile/ViewModels/ObsStatusViewModel.cs
+++ ObsControlMobile/ObsControlMobile/ViewModels/ObsStatusViewModel.cs
@@ -45,272 +45,177 @@ namespace ObsControlMobile.ViewModels
 
 
         #region OBSERVATORY 1 STATUS
-        double _ir1;
+        //IR
         public string Obs1_IR
         {
-            get {
-                return _ir1.ToString("0.0");
-            }
-            set {
-                 if (DoubleConversionUtils.TryParseToDouble(value, out _ir1)) 
-                        OnPropertyChanged();
-            }
+            get => ObsStatus1.ir.value.ToString("0.0");
         }
-        DateTime _ir1_updated;
+
         public string Obs1_IR_Updated
         {
-            get
-            {
-                return _ir1_updated.ToString("HH:mm:ss MM-dd-YYYY");
-            }
-            set
-            {
-                _ir1_updated=DateTime.Parse(value);
-                OnPropertyChanged();
-            }
+            get => ObsStatus1.ir.date.ToString("HH:mm:ss dd-MM-yyyy");
         }
-        double _humidity1;
+        public string Obs1_IR_Updated_Color
+        {
+            get => RecentColor(ObsStatus1.ir.ValueIsRecent);
+        }
+
+        //Humdiity
         public string Obs1_Humidity
         {
-            get
-            {
-                return _humidity1.ToString("0");
-            }
-            set
-            {
-                if (DoubleConversionUtils.TryParseToDouble(value, out _humidity1))
-                    OnPropertyChanged();
-            }
+            get => ObsStatus1.humidity.value.ToString("0.0");
         }
-        DateTime _humidity1_updated;
-        public string Humidity_Updated
+
+        public string Obs1_Humidity_Updated
         {
-            get
-            {
-                return _humidity1_updated.ToString("HH:mm:ss MM-dd-YYYY");
-            }
-            set
-            {
-                _humidity1_updated = DateTime.Parse(value);
-                OnPropertyChanged();
-            }
+            get => ObsStatus1.humidity.date.ToString("HH:mm:ss dd-MM-yyyy");
         }
-        double _inside1;
+        public string Obs1_Humidity_Updated_Color
+        {
+            get => RecentColor(ObsStatus1.humidity.ValueIsRecent);
+        }
+
+        //OBS2 Inside temp
         public string Obs1_Inside
         {
-            get
-            {
-                return _inside1.ToString("0.0");
-            }
-            set
-            {
-                if (DoubleConversionUtils.TryParseToDouble(value, out _inside1))
-                    OnPropertyChanged();
-            }
+            get => ObsStatus1.inside.value.ToString("0.0");
         }
-        DateTime _inside1_updated;
+
         public string Obs1_Inside_Updated
         {
-            get
-            {
-                return _inside1_updated.ToString("HH:mm:ss MM-dd-YYYY");
-            }
-            set
-            {
-                _inside1_updated = DateTime.Parse(value);
-                OnPropertyChanged();
-            }
+            get => ObsStatus1.inside.date.ToString("HH:mm:ss dd-MM-yyyy");
         }
-        double _akb1;
+        public string Obs1_Inside_Updated_Color
+        {
+            get => RecentColor(ObsStatus1.inside.ValueIsRecent);
+        }
+        //OBS1 AKB
         public string Obs1_Akb
         {
-            get
-            {
-                return _akb1.ToString("0.00");
-            }
-            set
-            {
-                if (DoubleConversionUtils.TryParseToDouble(value, out _akb1))
-                    OnPropertyChanged();
-            }
+            get => ObsStatus1.akb.value.ToString("0.0");
         }
-        DateTime _akb1_updated;
+
         public string Obs1_Akb_Updated
         {
-            get
-            {
-                return _akb1_updated.ToString("HH:mm:ss MM-dd-YYYY");
-            }
-            set
-            {
-                _akb1_updated = DateTime.Parse(value);
-                OnPropertyChanged();
-            }
+            get => ObsStatus1.akb.date.ToString("HH:mm:ss dd-MM-yyyy");
+        }
+        public string Obs1_Akb_Updated_Color
+        {
+            get => RecentColor(ObsStatus1.akb.ValueIsRecent);
         }
         #endregion OBSERVATORY 1 STATUS
 
         #region OBSERVATORY 2 STATUS
-        string _roofopen2;
         public string Obs2_Roof
         {
             get
             {
-                //if (_roofopen2 == "0") return "Closed";
-                //else if (_roofopen2 == "1") return "Opened";
-                //else return "Unknown";
                 if (ObsStatus2.roof.value == 0) return "Closed";
                 else if (ObsStatus2.roof.value == 1) return "Opened";
                 else return "Unknown";
             }
-            //set
-            //{
-            //    //SetProperty(ref _roofopen2, value);
-            //    _roofopen2 = value;
-            //    OnPropertyChanged();
-            //    OnPropertyChanged("Obs2_Roof_Color");
-            //}
         }
         public string Obs2_Roof_Color
         {
-            get
-            {
-                return (ObsStatus2.roof.value == 1 ? "lime" : (ObsStatus2.roof.value == 0 ? "silver" : "crimson"));
-            }
+            get=>(ObsStatus2.roof.value == 1 ? "lime" : (ObsStatus2.roof.value == 0 ? "silver" : "crimson"));
         }
-        DateTime _roof2_updated;
         public string Obs2_Roof_Updated
         {
-            get
-            {
-                //return _roof2_updated.ToString("HH:mm:ss dd-MM-yyyy");
-                return ObsStatus2.roof.date.ToString("HH:mm:ss dd-MM-yyyy");
-            }
-            //set
-            //{
-            //    _roof2_updated = DateTime.Parse(value);
-            //    OnPropertyChanged();
-            //    OnPropertyChanged("Obs2_Roof_Updated_Color");
-            //}
+            get=>ObsStatus2.roof.date.ToString("HH:mm:ss dd-MM-yyyy");
         }
         public string Obs2_Roof_Updated_Color
         {
-            get
-            {
-                //Debug.WriteLine("Here");
-                //string stout = JsonConvert.SerializeObject(ObsStatus2);
-                //Debug.WriteLine("Obj: " + stout);
-                //bool flag = ObsStatus2.roof.ValueIsRecent;
-
-                //double passed = (DateTime.Now - ObsStatus2.roof.date).TotalSeconds;
-                //bool flag2 = (passed < DateValuePair.VALID_DATETIME_MAX_SECONDS_SINCE_NOW);
-
-                //Debug.WriteLine("Date: "+ ObsStatus2.roof.date + ", _roof2_updated: "+ _roof2_updated+", Passed: " + passed + ", VALID_DATETIME_MAX_SECONDS_SINCE_NOW=" + DateValuePair.VALID_DATETIME_MAX_SECONDS_SINCE_NOW + ", flag:" + flag2 + ",orig: " + flag);
-
-                return (ObsStatus2.roof.ValueIsRecent ? "blue" : "crimson");
-            }
-        }
-
-        ObsStatusElement_Class ObsStatus2t = new ObsStatusElement_Class();
-        public DateValuePair Roof
-        {
-            get
-            {
-                return new DateValuePair() { value = 1.0, date = new DateTime(2010, 8, 18, 10, 10, 10)};
-            
-                //return ObsStatus2t.roof;
-            }
-            set
-            {
-                ObsStatus2t.roof.value = value.value;
-                ObsStatus2t.roof.date = value.date;
-
-
-                Debug.WriteLine("");
-                OnPropertyChanged();
-            }
+            get=>RecentColor(ObsStatus2.roof.ValueIsRecent);
         }
 
 
-        double _ir2;
+        //IR
         public string Obs2_IR
         {
-            get
-            {
-                return _ir2.ToString("0.0");
-            }
-            set
-            {
-                if (DoubleConversionUtils.TryParseToDouble(value, out _ir2))
-                    OnPropertyChanged();
-            }
+            get => ObsStatus2.ir.value.ToString("0.0");
         }
-        DateTime _ir2_updated;
+
         public string Obs2_IR_Updated
         {
-            get
-            {
-                return _ir2_updated.ToString("HH:mm:ss MM-dd-YYYY");
-            }
-            set
-            {
-                _ir2_updated = DateTime.Parse(value);
-                OnPropertyChanged();
-            }
+            get => ObsStatus2.ir.date.ToString("HH:mm:ss dd-MM-yyyy");
         }
-        double _humidity2;
+        public string Obs2_IR_Updated_Color
+        {
+            get=>RecentColor(ObsStatus2.ir.ValueIsRecent);
+        }
+
+        //Humdiity
         public string Obs2_Humidity
         {
-            get
-            {
-                return _humidity2.ToString("0");
-            }
-            set
-            {
-                if (DoubleConversionUtils.TryParseToDouble(value, out _humidity2))
-                    OnPropertyChanged();
-            }
+            get => ObsStatus2.humidity.value.ToString("0.0");
         }
-        DateTime _humidity2_updated;
-        public string Humidity2_Updated
+
+        public string Obs2_Humidity_Updated
         {
-            get
-            {
-                return _humidity2_updated.ToString("HH:mm:ss MM-dd-YYYY");
-            }
-            set
-            {
-                _humidity2_updated = DateTime.Parse(value);
-                OnPropertyChanged();
-            }
+            get => ObsStatus2.humidity.date.ToString("HH:mm:ss dd-MM-yyyy");
         }
-        double _inside2;
+        public string Obs2_Humidity_Updated_Color
+        {
+            get => RecentColor(ObsStatus2.humidity.ValueIsRecent);
+        }
+
+        //OBS2 Inside temp
         public string Obs2_Inside
         {
-            get
-            {
-                return _inside2.ToString("0.0");
-            }
-            set
-            {
-                if (DoubleConversionUtils.TryParseToDouble(value, out _inside2))
-                    OnPropertyChanged();
-            }
+            get => ObsStatus2.inside.value.ToString("0.0");
         }
-        DateTime _inside2_updated;
+
         public string Obs2_Inside_Updated
         {
-            get
-            {
-                return _inside2_updated.ToString("HH:mm:ss MM-dd-YYYY");
-            }
-            set
-            {
-                _inside2_updated = DateTime.Parse(value);
-                OnPropertyChanged();
-            }
+            get => ObsStatus2.inside.date.ToString("HH:mm:ss dd-MM-yyyy");
+        }
+        public string Obs2_Inside_Updated_Color
+        {
+            get => RecentColor(ObsStatus2.inside.ValueIsRecent);
         }
         #endregion OBSERVATORY 2 STATUS
 
+        public void RefreshBindingFields()
+        {
+            OnPropertyChanged("Obs1_Akb");
+            OnPropertyChanged("Obs1_Akb_Updated");
+            OnPropertyChanged("Obs1_Akb_Updated_Color");
+
+            OnPropertyChanged("Obs1_IR");
+            OnPropertyChanged("Obs1_IR_Updated");
+            OnPropertyChanged("Obs1_IR_Updated_Color");
+
+            OnPropertyChanged("Obs1_Humidity");
+            OnPropertyChanged("Obs1_Humidity_Updated");
+            OnPropertyChanged("Obs1_Humidity_Updated_Color");
+
+            OnPropertyChanged("Obs1_Inside");
+            OnPropertyChanged("Obs1_Inside_Updated");
+            OnPropertyChanged("Obs1_Inside_Updated_Color");
+
+
+            OnPropertyChanged("Obs2_Roof");
+            OnPropertyChanged("Obs2_Roof_Color");
+            OnPropertyChanged("Obs2_Roof_Updated");
+            OnPropertyChanged("Obs2_Roof_Updated_Color");
+
+            OnPropertyChanged("Obs2_IR");
+            OnPropertyChanged("Obs2_IR_Updated");
+            OnPropertyChanged("Obs2_IR_Updated_Color");
+
+            OnPropertyChanged("Obs2_Humidity");
+            OnPropertyChanged("Obs2_Humidity_Updated");
+            OnPropertyChanged("Obs2_Humidity_Updated_Color");
+
+            OnPropertyChanged("Obs2_Inside");
+            OnPropertyChanged("Obs2_Inside_Updated");
+            OnPropertyChanged("Obs2_Inside_Updated_Color");
+        }
+
+        public string RecentColor(bool IsRecent)
+        {
+            return (IsRecent ? "azure" : "crimson");
+        }
 
         #region Misc fileds
         string _currentdate;
@@ -356,10 +261,8 @@ namespace ObsControlMobile.ViewModels
                     ObsStatus2 = obsstatret.Item1["2"];
 
                     //REFRESH PROTPERTIES
-                    OnPropertyChanged("Obs2_Roof");
-                    OnPropertyChanged("Obs2_Roof_Color");
-                    OnPropertyChanged("Obs2_Roof_Updated");
-                    OnPropertyChanged("Obs2_Roof_Updated_Color");
+                    RefreshBindingFields();
+
 
 
 
